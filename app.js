391266411318a8211a65fbaeb5b2676f194a2ee6
vinit6626 <38699351+vinit6626@users.noreply.github.com>
2023-10-01 15:44:07 -0400

    Trendify project created

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const route = require('./routes/web.js');   
+const nodemailer = require('nodemailer');
+
+const bodyParser = require('body-parser');
+const path = require('path');
+const session = require('express-session');
+
+const app = express();
+app.use(session({
+  secret: 'Trendify', 
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(2700, ()=>{
+    console.log("Running at 2700");
+})
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+  });
+  
+app.use(route);
